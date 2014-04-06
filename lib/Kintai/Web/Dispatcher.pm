2ce4f33db4d6cf8d63acc168e5cc1d9763505f10
Makoto Sasaki <wanigoya@gmail.com>
2014-04-06 16:24:05 +0900

    土日祝日の表示対応

--- lib/Kintai/Web/Dispatcher.pm
+++ lib/Kintai/Web/Dispatcher.pm
@@ -4,6 +4,7 @@ use warnings;
 use utf8;
 use Time::Piece;
 use Amon2::Web::Dispatcher::RouterBoom;
+use Calendar::Japanese::Holiday;
 
 any '/' => sub {
     my ($c) = @_;
@@ -44,6 +45,8 @@ get '/kintai' => sub {
     my $user_id = $c->req->param('user_id');
     my $year_month = $c->req->param('year_month') // localtime->strftime("%Y%m");
 
+    my $month_table = create_month_table($year_month);
+
     my $kintai = $c->db->single(
         kintai => { user_id => $user_id, year_month => $year_month }
     );
@@ -67,23 +70,24 @@ get '/kintai' => sub {
     my @kintai;
     my $total_work_minutes = 0;
     while( my $row = $itr->next ) {
+
         my $break_minutes = calc_break_minutes( $row->attend_time, $row->leave_time );
         my $work_minutes = calc_work_minutes( $row->attend_time, $row->leave_time, $break_minutes );
         $total_work_minutes += $work_minutes;
-        push @kintai, {
-            date => format_date( $year_month.$row->day ),
-            attend_time => format_time($row->attend_time),
-            leave_time => format_time($row->leave_time),
-            break_time => format_jp_time_from_minutes( $break_minutes ),
-            work_time => format_jp_time_from_minutes( $work_minutes ),
-            remarks => $row->remarks,
-        };
+
+        my $month_row = $month_table->{ $year_month.$row->day };
+        $month_row->{date} = format_date( $year_month.$row->day );
+        $month_row->{attend_time} = format_time($row->attend_time);
+        $month_row->{leave_time} = format_time($row->leave_time);
+        $month_row->{break_time} = format_jp_time_from_minutes( $break_minutes );
+        $month_row->{work_time} = format_jp_time_from_minutes( $work_minutes );
+        $month_row->{remarks} = $row->remarks;
     }
 
     return $c->render('kintai.tx' => {
             user_id => $user_id,
             year_month => $year_month,
-            kintai => \@kintai,
+            kintai => $month_table,
             total_work_time => format_jp_time_from_minutes( $total_work_minutes ),
     });
 };
@@ -179,6 +183,38 @@ sub format_jp_time_from_minutes {
             sprintf( "%d時間", $minutes / 60, $minutes % 60 );
         }
     };
+}
+
+sub create_month_table {
+    my $year_month = shift;
+
+    my $year = substr( $year_month, 0, 4 );
+    my $month = substr( $year_month, 4, 2 );
+    my $last_day  = Time::Piece->strptime( $year.$month, '%Y%m' )->month_last_day;
+
+    my $month_table;
+    for my $day ( "01".."$last_day" ) {
+
+        my $date = Time::Piece->strptime( $year.$month.$day, '%Y%m%d' );
+        my $holiday = isHoliday( $year, $month+0, $day+0, 1 );
+        my $line_color = do {
+            if( $holiday or $date->wday == 1 ) {
+                "danger";
+            } elsif( $date->wday == 7 ) {
+                "info";
+            } else {
+                ""
+            }
+        };
+
+        $month_table->{$year.$month.$day} = {
+            date => format_date( $year.$month.$day ),
+            line_color => $line_color,
+            remarks => $holiday,
+        };
+    }
+
+    return $month_table;
 }
 
 sub format_date {
