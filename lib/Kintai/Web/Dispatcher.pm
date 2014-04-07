5a02f6830681be4031464384a3e2a47c9f60e620
Makoto Sasaki <wanigoya@gmail.com>
2014-04-08 00:57:40 +0900

    勤怠入力するときに年月入力を不要にする

--- lib/Kintai/Web/Dispatcher.pm
+++ lib/Kintai/Web/Dispatcher.pm
@@ -103,9 +103,7 @@ get '/kintai' => sub {
 post '/kintai' => sub {
     my ($c) = @_;
     my $user_id = $c->req->param('user_id');
-    my $date = $c->req->param('date');
-    my $year_month = substr($date,0,6);
-    my $day = substr($date,6,2);
+    my $year_month = $c->req->param('year_month');
 
     # TODO: トランザクション
 
@@ -132,7 +130,7 @@ post '/kintai' => sub {
     $c->db->insert(
         kintai_detail => {
             kintai_id => $kintai->id,
-            day => $day,
+            day => sprintf( "%02d", $c->req->param('day') ),
             attend_time => $c->req->param('attend_time'),
             leave_time => $c->req->param('leave_time'),
             remarks => $c->req->param('remarks'),
